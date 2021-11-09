1e6b83c15a4987bf2c458f857bd5f2ba3e46794a
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-11-09 10:14:47 -0500

    final commit

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author MB 11/09/2021
+
+# Question 1
+my_lst = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(my_lst))
+
+# Question 3
+print(my_lst[2])
+# Question 4
+print(5 in my_lst)
+
+# Question 5
+print(10 in my_lst)
+
+# Question 6
+lst = ['Adam', 'Ben', 'Chad', 'Mateo']
+
+# Question 7
+print(lst[3])
+
+# Question 8
+print(lst * 3)
+
+# Question 9
+alpha_lst = my_lst + lst
+print(alpha_lst)
+
+# Question 10
+print(len(alpha_lst))
